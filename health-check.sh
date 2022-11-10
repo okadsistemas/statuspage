b2305e1d4dc97b52375a22bc3ab508382a0c5941
Ennio Sousa <ennio21@gmail.com>
2022-11-10 10:52:58 -0300

    Update git user config

--- health-check.sh
+++ health-check.sh
@@ -59,8 +59,8 @@ done
 if [[ $commit == true ]]
 then
   # Let's make Vijaye the most productive person on GitHub.
-  git config --global user.name 'Vijaye Raji'
-  git config --global user.email 'vijaye@statsig.com'
+  git config --global user.name 'Ennio Sousa'
+  git config --global user.email 'ennio@enniosousa.com.br'
   git add -A --force logs/
   git commit -am '[Automated] Update Health Check Logs'
   git push
